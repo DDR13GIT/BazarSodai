805e008b422b8791d145baa9146b1f39e91f17b8
Noumi <isratmoyeen.23@gmail.com>
2022-07-25 01:45:34 +0600

    Login_Signup

---
+++ BazarSodai/Controllers/LogInTableController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BazarSodai.Controllers
+{
+    public class LogInTableController : Controller
+    {
+
+        ShopDatabaseEntities shopdata = new ShopDatabaseEntities();
+        // GET: LogInTable
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login([Bind(Include = "Email, Password")] LogInTable user)
+        {
+            List<LogInTable> userAccounts = shopdata.LogInTable.Where(temp => temp.Email == user.Email &&
+            temp.Password == user.Password).ToList();
+            while (userAccounts.Count > 0)
+            {
+                Console.WriteLine("hellllllooo");
+                return RedirectToAction("Index", "LogInTable");
+
+               // Response.Redirect("https://localhost:44375/LogInTable/Index");
+
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CreateAccount()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult CreateAccount([Bind(Include = "Email, PhoneNo, Password")] LogInTable user)
+        {
+            if (ModelState.IsValid)
+            {
+              shopdata.LogInTable.Add(user);
+                shopdata.SaveChanges();
+                Response.Redirect("https://localhost:44375/LogInTable/Login");
+                return View();
+            }
+            return View();
+        }
+
+    }
+}
